fd134c6eba399d6ac87dcd15deb4366ab7a0f8d5
primozgodec <p.godec9@gmail.com>
2017-07-31 14:17:41 +0200

    Test

--- src/test/test.py
+++ src/test/test.py
@@ -63,6 +63,8 @@ def color_images_full(model, name, b_size=32):
             # workaround for not suitable shape
             colored_im = resize_image(np.concatenate((all_images_l[i], color_im[i]), axis=2), (h, w), "LAB")
 
+            print(original_im_bw[:, :, np.newaxis].shape)
+            print(colored_im[:, :, 1:].shape)
             lab_im = np.concatenate((original_im_bw[:, :, np.newaxis], colored_im[:, :, 1:]), axis=2)
             im_rgb = color.lab2rgb(lab_im)
 
