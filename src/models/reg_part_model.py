2e61fbf1356b288a00096fb0def49a49f631456c
primozgodec <p.godec9@gmail.com>
2017-08-01 10:37:20 +0200

    Regression part docstring

--- src/models/reg_part_model.py
+++ src/models/reg_part_model.py
@@ -15,6 +15,14 @@ color_fun = color_images_part
 
 
 def model():
+    """
+    Function build and returns model for approach regression part
+
+    Returns
+    -------
+    keras.engine.training.Model
+        Keras model for approach
+    """
     main_input = Input(shape=input_shape, name='image_part_input')
 
     x = Conv2D(64, (3, 3), padding="same", activation="relu",
