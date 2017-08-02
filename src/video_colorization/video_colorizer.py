cc1e673a4fa26fee50ee39c0692418cfdf7bea55
primozgodec <p.godec9@gmail.com>
2017-08-02 15:17:43 +0200

    Video set output frame rate

--- src/video_colorization/video_colorizer.py
+++ src/video_colorization/video_colorizer.py
@@ -91,7 +91,7 @@ def color_one_video(model, video, b_size=32):
     # open reader and writer
     videogen = skvideo.io.vreader(os.path.join(get_abs_path(source_dir), video))
     videowriter = skvideo.io.FFmpegWriter(os.path.join(get_abs_path(destination_dir), video),
-                                          inputdict={"-r": frame_rate},
+                                          # inputdict={"-r": frame_rate},
                                           outputdict={"-r": frame_rate})
 
     # progress bar
